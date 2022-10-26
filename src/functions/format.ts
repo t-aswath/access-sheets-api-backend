e6e8ad22f8a5f754e5aef1aac361537f5fc0fdef
RahulNavneeth <rahulmnavneeth@gmail.com>
2022-10-26 17:32:33 +0530

    limit

--- src/functions/format.ts
+++ src/functions/format.ts
@@ -8,18 +8,21 @@ export const format = (
   switch (type) {
     case "KEY_VALUE":
       const keys = data[0];
-      return data.slice(1).map((i) => {
-        return parse(i, keys);
-      });
+      return data
+        .slice(1)
+        .map((i) => {
+          return parse(i, keys);
+        })
+        .slice(0, lim);
 
     case "LIST":
-      return data;
+      return data.splice(0, lim);
     case "KEY_PAIR":
       const pair = {};
       for (let i = 0; i < data[0].length; i++) {
         pair[data[0][i]] = [];
       }
-      for (const i of data.slice(1)) {
+      for (const i of data.slice(1).slice(0, lim)) {
         for (const [key, value] of Object.entries(i)) {
           pair[data[0][key]].push(value);
         }
